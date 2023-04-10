9ebffb935d4128e1315008cc8f8c860448b71ec0
Sergey <sergey-707@mail.ru>
2023-04-10 18:13:43 +0400

    login form correction

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,8 @@ const Login = () => {
             <div className="w-full max-w-sm p-6 m-auto mx-auto bg-white rounded-lg shadow-md mt-28">
                 <div className="flex justify-center mx-auto">
                     {/*<img className="w-auto h-7 sm:h-8" src="https://merakiui.com/images/logo.svg" alt=""/>*/}
-                    <h3 className="mt-3 text-xl font-medium text-center text-gray-600 ">Войти в аккаунт</h3>
+                    {/*<h3 className="mt-3 text-xl font-medium text-center text-gray-600 ">Войти в аккаунт</h3>*/}
+                    <h3 className="mt-3 text-xl font-medium text-center text-gray-600 ">Войдите в аккаунт</h3>
                 </div>
 
                 <form className="mt-6" onSubmit={login}>
@@ -23,27 +24,30 @@ const Login = () => {
                         <label htmlFor="email"
                                className="block text-sm text-gray-800 ">Email</label>
                         <input type="email"
-                               required
+                               autoComplete="off"
+                               // required
                                className="border-gray-300 block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"/>
                     </div>
 
                     <div className="mt-4">
                         <div className="flex items-center justify-between">
                             <label htmlFor="password"
-                                   className="block text-sm text-gray-800">Password</label>
+                                   className="block text-sm text-gray-800">Пароль</label>
                             {/*<a href="#" className="text-xs text-gray-600 dark:text-gray-400 hover:underline">Forget*/}
                             {/*    Password?</a>*/}
                         </div>
 
                         <input type="password"
-                               required
+                               autoComplete="off"
+                               // required
                                className="border-gray-300 block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg  focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"/>
                     </div>
 
                     <div className="mt-6">
                         <button
                             className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
-                            Sign In
+                            {/*Sign In*/}
+                            Войти
                         </button>
                     </div>
                 </form>
@@ -80,8 +84,8 @@ const Login = () => {
                 {/*    </a>*/}
                 {/*</div>*/}
 
-                <p className="mt-8 text-xs font-light text-center text-gray-400"> Don't have an account?
-                    <a href="#" className="font-medium text-gray-700 hover:underline"> Create One</a>
+                <p className="mt-8 text-xs font-light text-center text-gray-400"> Еще нет аккаунта?
+                    <a href="#" className="font-medium text-gray-700 hover:underline"> Создать</a>
                 </p>
             </div>
         // </div>
